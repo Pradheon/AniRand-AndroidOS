2beec7038cf39f46fdfafab605cddc6f67087931
Pradheon <pradheon@gmail.com>
2021-12-12 20:36:48 -0800

    Added expandableListView to Anime Tracker and Manga Tracker.

--- AniRand/app/src/main/java/usv/edu/jrai/ui/animetracker/AnimeTrackerFragment.kt
+++ AniRand/app/src/main/java/usv/edu/jrai/ui/animetracker/AnimeTrackerFragment.kt
@@ -1,22 +1,26 @@
 package usv.edu.jrai.ui.animetracker
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListAdapter
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import usv.edu.jrai.R
+import usv.edu.jrai.CustomExpandableListAdapter
 import usv.edu.jrai.databinding.FragmentAnimetrackerBinding
-import usv.edu.jrai.databinding.FragmentDashboardBinding
-import usv.edu.jrai.ui.dashboard.DashboardViewModel
 
 class AnimeTrackerFragment : Fragment() {
 
+    val applicationContext: Context? = null
     private lateinit var animeTrackerViewModel: AnimeTrackerViewModel
     private var _binding: FragmentAnimetrackerBinding? = null
+    private var _adapter: ExpandableListAdapter? = null
+    private var titleList: List<String>? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -33,6 +37,8 @@ class AnimeTrackerFragment : Fragment() {
         _binding = FragmentAnimetrackerBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        setupExpandableListView()
+
         val textView: TextView = binding.textAnimeTracker
         animeTrackerViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
@@ -44,4 +50,44 @@ class AnimeTrackerFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    /** The code below was created following the tutorials below
+     * Tutorial 1: https://www.tutorialspoint.com/how-to-create-a-expandable-listview-using-kotlin
+     * Tutorial 2: https://tutorialwing.com/android-expandablelistview-using-kotlin-example/
+     * **/
+    private fun setupExpandableListView() {
+        val expandableListView = binding.expandableListView
+        val listData = animeTrackerViewModel.data
+        titleList = ArrayList(listData.keys)
+        _adapter = CustomExpandableListAdapter(requireContext(), titleList as ArrayList<String>, listData)
+        expandableListView.setAdapter(_adapter)
+
+        expandableListView.setOnGroupExpandListener { groupPosition ->
+            Toast.makeText(
+                requireContext(),
+                (titleList as ArrayList<String>)[groupPosition] + " List Expanded.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        expandableListView.setOnGroupCollapseListener { groupPosition ->
+            Toast.makeText(
+                requireContext(),
+                (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        expandableListView.setOnChildClickListener { parent, v, groupPosition, childPosition, id ->
+            Toast.makeText(
+                requireContext(),
+                "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(titleList as ArrayList<String>)[groupPosition]]!!.get(
+                    childPosition
+                ),
+                Toast.LENGTH_SHORT
+            ).show()
+            false
+        }
+    }
+
 }
--- AniRand/app/src/main/java/usv/edu/jrai/ui/animetracker/AnimeTrackerViewModel.kt
+++ AniRand/app/src/main/java/usv/edu/jrai/ui/animetracker/AnimeTrackerViewModel.kt
@@ -12,4 +12,134 @@ class AnimeTrackerViewModel : ViewModel() {
         value = "Anime"
     }
     val text: LiveData<String> = _text
+
+    /** This code segment below was created following the tutorials below
+     * Tutorial 1: https://www.tutorialspoint.com/how-to-create-a-expandable-listview-using-kotlin
+     * Tutorial 2: https://tutorialwing.com/android-expandablelistview-using-kotlin-example/
+     * **/
+    val data: HashMap<String, List<String>>
+        get() {
+            val listData = HashMap<String, List<String>>()
+
+            val watching = ArrayList<String>()
+            watching.add("Jobless Reincarnation: Part 2")
+            watching.add("Demon Slayer: Mugen Train (TV)")
+            watching.add("Jujutsu Kaisen 0")
+            watching.add("Komii-san Can't Communicate")
+            watching.add("Platinum End")
+            watching.add("Takt op.Destiny")
+
+            val completed = ArrayList<String>()
+            completed.add("Jobless Reincarnation: Part 2")
+            completed.add("Demon Slayer: Mugen Train (TV)")
+            completed.add("Jujutsu Kaisen 0")
+            completed.add("Komii-san Can't Communicate")
+            completed.add("Platinum End")
+            completed.add("Takt op.Destiny")
+
+            val paused = ArrayList<String>()
+            paused.add("Jobless Reincarnation: Part 2")
+            paused.add("Demon Slayer: Mugen Train (TV)")
+            paused.add("Jujutsu Kaisen 0")
+            paused.add("Komii-san Can't Communicate")
+            paused.add("Platinum End")
+            paused.add("Takt op.Destiny")
+
+            val dropped = ArrayList<String>()
+            dropped.add("Jobless Reincarnation: Part 2")
+            dropped.add("Demon Slayer: Mugen Train (TV)")
+            dropped.add("Jujutsu Kaisen 0")
+            dropped.add("Komii-san Can't Communicate")
+            dropped.add("Platinum End")
+            dropped.add("Takt op.Destiny")
+
+            val planning = ArrayList<String>()
+            planning.add("Jobless Reincarnation: Part 2")
+            planning.add("Demon Slayer: Mugen Train (TV)")
+            planning.add("Jujutsu Kaisen 0")
+            planning.add("Komii-san Can't Communicate")
+            planning.add("Platinum End")
+            planning.add("Takt op.Destiny")
+
+            listData["Watching"] = watching
+            listData["Completed"] = completed
+            listData["Paused"] = paused
+            listData["Dropped"] = dropped
+            listData["Planning"] = planning
+
+            return listData
+        }
+
+    /**
+    private var watching: MutableList<String> = ArrayList()
+    private var completed: MutableList<String> = ArrayList()
+    private var paused: MutableList<String> = ArrayList()
+    private var dropped: MutableList<String> = ArrayList()
+    private var planning: MutableList<String> = ArrayList()
+
+    private fun getData(): HashMap<String, List<String>> {
+        listData["Watching"] = getWatchingList()
+        listData["Completed"] = getCompletedList()
+        listData["Paused"] = getPausedList()
+        listData["Dropped"] = getDroppedList()
+        listData["Planning"] = getPlanningList()
+        return listData
+    }**/
+
+    /**
+    private fun getWatchingList(): String {
+        watching = mutableListOf(
+            "Jobless Reincarnation: Part 2",
+            "Demon Slayer: Mugen Train (TV)",
+            "Jujutsu Kaisen 0",
+            "Komii-san Can't Communicate",
+            "Platinum End",
+            "Takt op.Destiny"
+        )
+        return watching
+    }
+
+    private fun getCompletedList(): String {
+        completed = mutableListOf(
+            "Jobless Reincarnation: Part 2",
+            "Demon Slayer: Mugen Train (TV)",
+            "Jujutsu Kaisen 0",
+            "Komii-san Can't Communicate",
+            "Platinum End",
+            "Takt op.Destiny"
+        )
+    }
+
+    private fun getPausedList(): String {
+        paused = mutableListOf(
+            "Jobless Reincarnation: Part 2",
+            "Demon Slayer: Mugen Train (TV)",
+            "Jujutsu Kaisen 0",
+            "Komii-san Can't Communicate",
+            "Platinum End",
+            "Takt op.Destiny"
+        )
+    }
+
+    private fun getDroppedList(): String {
+        dropped = mutableListOf(
+            "Jobless Reincarnation: Part 2",
+            "Demon Slayer: Mugen Train (TV)",
+            "Jujutsu Kaisen 0",
+            "Komii-san Can't Communicate",
+            "Platinum End",
+            "Takt op.Destiny"
+        )
+    }
+
+    private fun getPlanningList(): String {
+        planning = mutableListOf(
+            "Jobless Reincarnation: Part 2",
+            "Demon Slayer: Mugen Train (TV)",
+            "Jujutsu Kaisen 0",
+            "Komii-san Can't Communicate",
+            "Platinum End",
+            "Takt op.Destiny"
+        )
+    }**/
 }
